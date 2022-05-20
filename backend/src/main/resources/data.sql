ab177756fd7736c1d9e3f9444e48dc91da2a1ac0
Alexsandro Machado <lekomachado@hotmail.com>
2022-05-20 14:50:47 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) Values ('Bootcamp HTML', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQYmAsy2_oPtcTkdviJW09ld3_72YOmzl4iP20Ber9AmI-QVMFLV54mahqFUw_N1fyz0&usqp=CAU'); 
